39fa6804407d3b6a8012ec8875c6ad4ebd11522d
SlaunchaMan <SlaunchaMan@7f27801e-05d9-11df-a276-edb32ab3677a>
2010-01-20 16:08:04 +0000

    Added PackageMaker files.

--- trunk/main.c
+++ trunk/main.c
@@ -1,3 +1,28 @@
+//
+//  main.c
+//  detectdisplays
+//
+//  Created by Jeff Kelley on January 20, 2010.
+//  Copyright (c) 2010 Regents of The University of Michigan.
+//  All Rights Reserved.
+//
+//  Permission to use, copy, modify, and distribute this software and its
+//  documentation for any purpose and without fee is hereby granted, provided
+//  that the above copyright notice appears in all copies and that both that
+//  copyright notice and this permission notice appear in supporting
+//  documentation, and that the name of The University of Michigan not be used
+//  in advertising or publicity pertaining to distribution of the software
+//  without specific, written prior permission. This software is supplied as is
+//  without expressed or implied warranties of any kind.
+//
+//  Campus Computing Sites
+//  The University of Michigan
+//  c/o Jeff Kelley
+//  B622A Michigan Union
+//  530 S. State St.
+//  Ann Arbor, MI 48109
+//
+
 #include <ApplicationServices/ApplicationServices.h>
 #include <IOKit/IOKitLib.h>
 #include <IOKit/IOTypes.h>
